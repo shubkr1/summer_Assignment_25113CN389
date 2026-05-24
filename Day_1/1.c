58d308333b6b01574ebabcc0e9c2b61984525f74
Shubham Kumar Singh <shubham2082003@gmail.com>
2026-05-24 14:38:32 +0530

    newnew

--- Day_1/1.c
+++ Day_1/1.c
@@ -1,1 +1,12 @@
-ahh ahh 
+#include<stdio.h>
+int main() 
+{
+int n,sum=0;
+printf("Enter the number:");
+scanf("%d",&n);
+for(int i=1; i<=n; i++){
+    sum +=i;
+}
+printf("The sum of first %d natural number is: %d\n",n,sum);
+return 0;
+}
