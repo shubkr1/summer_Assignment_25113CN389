af33893b98afe20f807afd19f8308a7ce7299d0f
Shubham Kumar Singh <shubham2082003@gmail.com>
2026-05-31 23:23:28 +0530

    newnew

---
+++ Day_4/15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, temp, rem, sum = 0;
+
+    scanf("%d", &n);
+    temp = n;
+
+    while (temp != 0) {
+        rem = temp % 10;
+        sum += rem * rem * rem;
+        temp /= 10;
+    }
+
+    if (sum == n)
+        printf("Armstrong Number");
+    else
+        printf("Not Armstrong Number");
+
+    return 0;
+}
